0cf5324cbb1e1f7ecdee269731baaf69587598fc
Martin Briones <brionen@ph.ibm.com>
2018-02-12 18:10:36 +0800

    Sample

--- HelloWorldForTraining/src/main/java/com/ibm/training/Test.java
+++ HelloWorldForTraining/src/main/java/com/ibm/training/Test.java
@@ -2,10 +2,17 @@ package com.ibm.training;
 
 public class Test {
 
+	public static String str = "This is a string..";
+	
 	public static void main(String[] args) {
 		System.out.println ("Hello Github");
 		//test
 		//test again
+		System.out.println (writeSomething());
+	}
+	
+	public static String writeSomething() {
+		return str;
 	}
 	
 	public void sampleMethod() {
