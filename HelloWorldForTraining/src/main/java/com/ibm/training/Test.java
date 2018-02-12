752915df8b51be39abfc87ddd12d5ca1282aa256
marjay <dacanam@ph.ibm.com>
2018-02-12 19:05:51 +0800

    Hello World - Dacanay

--- HelloWorldForTraining/src/main/java/com/ibm/training/Test.java
+++ HelloWorldForTraining/src/main/java/com/ibm/training/Test.java
@@ -31,6 +31,7 @@ public class Test {
 	public void sampleMethod() {
 		System.out.println("keepitsimpol");
 		System.out.println("permission to merge with master");
+		System.out.println("hello Wolrd");
 	}
 	
 	public String samplePush() {
